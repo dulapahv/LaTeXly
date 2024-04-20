af597256f2d417fd461e89a00910c1da70434444
Dulapah Vibulsanti <dulapah6@gmail.com>
2024-04-20 15:06:01 +0100

    refactor: refactor code and add comments

--- src/components/SymBtn.tsx
+++ src/components/SymBtn.tsx
@@ -2,6 +2,7 @@
  * Button component for inserting symbols into the editor
  *
  * References:
+ * https://codepen.io/Fantantonio/pen/oNdreeB
  * https://stackoverflow.com/a/34968263/17302377
  */
 
@@ -23,28 +24,37 @@ const SymBtn = ({
   caretPosition = value.length,
   isBlockMath = false,
 }: SymBtnProps) => {
-  const { setEquation: setExpression } = useEditor();
+  const { setEquation } = useEditor();
+
+  const handleInsert = (value: string) => {
+    const editor = document.getElementById('editor') as HTMLTextAreaElement;
+    if (!editor) return;
+
+    editor.focus();
+
+    // Insert the symbol at the caret position
+    const start = editor.selectionStart;
+    const end = editor.selectionEnd;
+    const textBefore = editor.value.substring(0, start);
+    const textAfter = editor.value.substring(end, editor.value.length);
+
+    // Update the editor value
+    editor.value = textBefore + value + textAfter;
+    setEquation(editor.value);
+
+    // Move the caret to the caretPosition
+    editor.selectionStart = start + caretPosition;
+    editor.selectionEnd = start + caretPosition;
+  };
 
   return (
     <Button
+      onPress={() => handleInsert(text)}
+      className="text-base"
       isIconOnly
       size="sm"
       variant="light"
       aria-label={text}
-      className="text-base"
-      onClick={() => {
-        const editor = document.getElementById('editor') as HTMLTextAreaElement;
-        if (!editor) return;
-        editor.focus();
-        const start = editor.selectionStart;
-        const end = editor.selectionEnd;
-        const textBefore = editor.value.substring(0, start);
-        const textAfter = editor.value.substring(end, editor.value.length);
-        editor.value = textBefore + value + textAfter;
-        setExpression(editor.value);
-        editor.selectionStart = start + caretPosition;
-        editor.selectionEnd = start + caretPosition;
-      }}
     >
       {isBlockMath ? <BlockMath math={text} /> : <InlineMath math={text} />}
     </Button>
