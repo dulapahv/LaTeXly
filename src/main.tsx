6705e06be6ad8f6e8bbb1fe91844d381b9613609
Dulapah Vibulsanti <dulapah6@gmail.com>
2026-03-19 23:55:01 +0000

    fix: add missing export and broken git revert

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { App } from "./App";
+
+import "./globals.css";
 import "@fontsource-variable/geist";
 import "@fontsource-variable/geist-mono";
-import "./globals.css";
-import { App } from "./App";
+
 
 const root = document.getElementById("root");
 if (!root) throw new Error("Root element not found");
